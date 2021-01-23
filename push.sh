f44ff9030ff8f15bb0b2f39e8faee86697170cff
ibuibu <ibuibu69@gmail.com>
2021-01-23 22:27:06 +0900

    Add Code

--- push.sh
+++ push.sh
@@ -6,6 +6,10 @@ if [ $# -eq 0 ]; then
 fi
 
 cp ../.zshrc ./
+cp ~/Library/Application\ Support/Code/User/settings.json ./.config/Code/User/
+cp ~/Library/Application\ Support/Code/User/keybindings.json ./.config/Code/User/
+cp ~/Library/Application\ Support/Code/User/tasks.json ./.config/Code/User/
+cp -r ~/Library/Application\ Support/Code/User/snippets ./.config/Code/User/
 cp ../.gitignore_global ./
 cp -r ../.command ./
 
