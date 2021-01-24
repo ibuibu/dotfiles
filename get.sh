caeb2cb6d20f0ebf731096044c694ada054301db
ibuibu <ibuibu69@gmail.com>
2021-01-24 22:03:42 +0900

    change push to get shellscript

--- get.sh
+++ get.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-  echo "Set commit message via argument."
-  exit 1
-fi
-
-cp ../.zshrc ./
+cp ~/.zshrc ./
 cp ~/Library/Application\ Support/Code/User/settings.json ./.config/Code/User/
 cp ~/Library/Application\ Support/Code/User/keybindings.json ./.config/Code/User/
 cp ~/Library/Application\ Support/Code/User/tasks.json ./.config/Code/User/
 cp -r ~/Library/Application\ Support/Code/User/snippets ./.config/Code/User/
-cp ../.gitignore_global ./
-cp -r ../.command ./
-
-git add .
-git commit -m "$@"
-git push
+cp ~/.gitignore_global ./
+cp -r ~/.command ./
