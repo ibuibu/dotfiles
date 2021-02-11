8644f4d2b4fe531816efdae79ebda87242b676c8
ibuibu <ibuibu69@gmail.com>
2021-02-11 21:26:56 +0900

    Add tmux.conf and config of iterm2

--- get.sh
+++ get.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-cp ~/.zshrc ./
 cp -r ~/.config/nvim ./.config/
 cp ~/Library/Application\ Support/Code/User/settings.json ./.config/Code/User/
 cp ~/Library/Application\ Support/Code/User/keybindings.json ./.config/Code/User/
 cp ~/Library/Application\ Support/Code/User/tasks.json ./.config/Code/User/
 cp -r ~/Library/Application\ Support/Code/User/snippets ./.config/Code/User/
-cp ~/.gitignore_global ./
-cp -r ~/.command ./
-
-brew bundle dump --force
-npm ls -g --depth=0 > ./npm_global.txt
-yarn global list --depth=0 > ./yarn_global.txt
 
 code --list-extensions > ./.config/Code/extensions
+npm ls -g --depth=0 > ./npm_global.txt
+yarn global list --depth=0 > ./yarn_global.txt
+brew bundle dump --force
